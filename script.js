b238566ef567181fe67e9c4d44237ed64d95e750
Shamsuzzoha <33594007+ArbiKhan007@users.noreply.github.com>
2025-11-14 10:48:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-	const ul=document.querySelector("#ul_1");
+    const bandsList = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+	const bands=document.querySelector("#bands");
 	
 	//console.log(ul);
 	
-	const bandsSorted=bands.sort(function (a,b) {
+	bandsList.sort(function (a,b) {
 		let aArr=a.split(" ");
 		let bArr=b.split(" ");
 		let str1=aArr[0];
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		}
 	});
 	
-	bands.map((band)=>{
-		ul.insertAdjacentHTML("beforeend", `<li>${band}</li>`);
+	bandsList.map((band)=>{
+		bands.insertAdjacentHTML("beforeend", `<li>${band}</li>`);
 	})
 });
